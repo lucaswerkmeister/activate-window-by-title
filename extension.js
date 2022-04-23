c2785a88798c70e6ea0ada86ee0e0693e4fc96ec
Lucas Werkmeister <mail@lucaswerkmeister.de>
2022-04-23 16:21:35 +0200

    Implement extension
    Loosely based on a StackOverflow answer by Michal Rus [1]. Example use:
    
        busctl --user call \
            org.gnome.Shell \
    	/de/lucaswerkmeister/ActivateWindowByTitle \
    	de.lucaswerkmeister.ActivateWindowByTitle \
    	activateBySubstring \
    	s 'Firefox'
    
    [1]: https://stackoverflow.com/a/33002516/1420237

--- extension.js
+++ extension.js
@@ -18,17 +18,94 @@
 
 /* exported init */
 
-class Extension {
+const { Gio } = imports.gi;
+
+const ActivateWindowByTitleInterface = `
+<node>
+  <interface name="de.lucaswerkmeister.ActivateWindowByTitle">
+    <method name="activateByTitle">
+      <arg name="fullTitle" type="s" direction="in" />
+      <arg name="found" type="b" direction="out" />
+    </method>
+    <method name="activateByPrefix">
+      <arg name="prefix" type="s" direction="in" />
+      <arg name="found" type="b" direction="out" />
+    </method>
+    <method name="activateBySuffix">
+      <arg name="suffix" type="s" direction="in" />
+      <arg name="found" type="b" direction="out" />
+    </method>
+    <method name="activateBySubstring">
+      <arg name="substring" type="s" direction="in" />
+      <arg name="found" type="b" direction="out" />
+    </method>
+  </interface>
+</node>
+`;
+
+class ActivateWindowByTitle {
+    #dbus;
+
     constructor() {
+        this.#dbus = Gio.DBusExportedObject.wrapJSObject(
+            ActivateWindowByTitleInterface,
+            this,
+        );
     }
 
     enable() {
+        this.#dbus.export(
+            Gio.DBus.session,
+            '/de/lucaswerkmeister/ActivateWindowByTitle',
+        );
     }
 
     disable() {
+        this.#dbus.unexport_from_connection(
+            Gio.DBus.session,
+        );
     }
+
+    #activateByPredicate(predicate) {
+        for (const actor of global.get_window_actors()) {
+            const window = actor.get_meta_window(),
+                title = window.get_title();
+            if (predicate(title)) {
+                window.activate(global.get_current_time());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    activateByTitle(fullTitle) {
+        return this.#activateByPredicate(
+            (title) => title === fullTitle,
+        );
+    }
+
+    activateByPrefix(prefix) {
+        return this.#activateByPredicate(
+            (title) => title.startsWith(prefix),
+        );
+    }
+
+    activateBySuffix(suffix) {
+        return this.#activateByPredicate(
+            (title) => title.endsWith(suffix),
+        );
+    }
+
+    activateBySubstring(substring) {
+        return this.#activateByPredicate(
+            (title) => title.includes(substring),
+        );
+    }
+
+    // note: we don’t offer activateByRegExp,
+    // because that would be vulnerable to ReDoS attacks
 }
 
 function init() {
-    return new Extension();
+    return new ActivateWindowByTitle();
 }
